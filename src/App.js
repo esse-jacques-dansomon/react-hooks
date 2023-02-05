bb1a10b649ed18657e191b840fe2dc42777781d8
Esse Dansomon <essedansomon@gmail.com>
2023-02-05 20:07:55 +0000

    end use ref useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useReducer} from "react";
+
+const initialState = {count: 0};
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increase':
+            return {count: state.count + 1};
+        case 'decrease':
+            return {count: state.count - 1};
+        default:
+            return state;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const increment = () => {dispatch({type : 'increase'})};
+    const decrement = () => {dispatch({type : "decrease"})};
 
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <p>count : {state.count} </p>
+            <div className="">
+                <button onClick={increment}>increase</button>
+                <button onClick={decrement}>decrease</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
