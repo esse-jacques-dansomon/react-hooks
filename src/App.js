0c2e7e4585a9364136c0ec8581bf419d82bec496
Esse Dansomon <essedansomon@gmail.com>
2023-02-05 18:46:49 +0000

    end use ref hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
+    const [count, setCount] = useState(0);
+    const refCount = useRef(0);
+    useEffect(() => {
+        return () => {
+            console.log('refCount.current', refCount.current);
+            console.log('count', count);
+        };
+    });
+    const inputEle = useRef(null);
+    const increment = () => setCount(prev => prev + 1);
 
-    </div>
-  );
+    function handleClick() {
+        increment();
+        refCount.current++;
+        inputEle.current.focus();
+    }
+
+    return (
+        <div className="App">
+            <p>{count} times</p>
+            <p>{refCount.current} times</p>
+            <button onClick={handleClick}>Click me</button>
+            <input ref={inputEle}/>
+        </div>
+    );
 }
 
 export default App;
