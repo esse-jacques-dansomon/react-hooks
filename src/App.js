7399162547ad9b34abdf8c815e91b8afd26b9039
Esse Dansomon <essedansomon@gmail.com>
2023-02-05 20:19:46 +0000

    end use ref useEffectLayout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
+
+//useLayoutEffect is a hook that runs after every render.
+// is used to read the DOM layout and synchronously re-render.
+// It is a close cousin of useEffect.  The difference is that effects
+// scheduled with useLayoutEffect will be flushed synchronously,
+// before the browser has a chance to paint.
 import './App.css';
+import {useEffect, useLayoutEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("useEffect");
+  });
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect");
+  });
   return (
     <div className="App">
 
